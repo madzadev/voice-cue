5ee84fdef9c5b10fe8c2b8ac8c497c914a25c3c0
madzadev <madars.biss@gmail.com>
2022-03-28 17:36:10 +0200

    edit tag cloud list

--- components/TagCloud.js
+++ components/TagCloud.js
@@ -1,10 +1,30 @@
 import { TagCloud } from "react-tagcloud";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { transcription } from "../data/transcription";
 
 function SimpleCloud({ data }) {
   const [activeTag, setActiveTag] = useState();
+  const [occurrences, setOccurrences] = useState([]);
+  const [tagList, setTagList] = useState();
+
+  function getTagList(occ) {
+    return occ.map((el, index) => {
+      return (
+        <div>
+          <h3>
+            #{index + 1} appears at {el.start}
+          </h3>
+          <p>This is the text included</p>
+        </div>
+      );
+    });
+  }
+
+  useEffect(() => {
+    setTagList(getTagList(occurrences));
+  }, [occurrences]);
+
   return (
     <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
       <TagCloud
@@ -12,25 +32,22 @@ function SimpleCloud({ data }) {
         maxSize={35}
         tags={data}
         onClick={(tag) => {
-          console.log(
-            `'${tag.value}' was selected! Occurred ${tag.count} times`
-          );
           setActiveTag(tag);
+          setOccurrences([]);
+          transcription.words.forEach((el, index) => {
+            if (tag.value === el.word) {
+              console.log(tag);
+              setOccurrences((occurrences) => [...occurrences, el]);
+            }
+          });
         }}
       />
       <div>
         {activeTag ? (
           <>
-            <h1>Selected word: {activeTag.value}</h1>
+            <h1>Word: {activeTag.value}</h1>
             <h3>Occurred {activeTag.count} times</h3>
-            {transcription.words.map((el, index) => {
-              console.log(el.word);
-              if (activeTag.value === el.word) {
-                <p key={index}>
-                  Occurrence {index + 1}: {el.word} at {el.start}
-                </p>;
-              }
-            })}
+            {tagList}
           </>
         ) : (
           <h1>Select a tag to get analysis</h1>
