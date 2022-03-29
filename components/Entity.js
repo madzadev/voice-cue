6dcd88904f8cf5edd51731da2ef804218c74a56f
madzadev <madars.biss@gmail.com>
2022-03-29 20:33:19 +0200

    add entities

--- components/Entity.js
+++ components/Entity.js
@@ -13,9 +13,21 @@ const Entity = () => {
     <ViewSplitter>
       <div>
         <h1>This is entity</h1>
+        <h3>Person</h3>
+        <h3>Place</h3>
+        <h3>Money</h3>
+        <h3>Date</h3>
+        <h3>Url</h3>
+        <h3>Email</h3>
+        <h3>Organization</h3>
+        <h3>NumericValue</h3>
+        <h3>HashTag</h3>
+        <h3>PhoneNumber</h3>
+        <h3>Unit</h3>
+        <h3>Activity</h3>
       </div>
       <div>
-        <h1>Select smth</h1>
+        <h1>Select entity to get cues</h1>
       </div>
     </ViewSplitter>
   );
