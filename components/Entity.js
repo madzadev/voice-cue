e01f58119479ba4ca9dbf8b21b3e2ce8dc7807c1
madzadev <madars.biss@gmail.com>
2022-03-29 20:19:13 +0200

    edit entity

--- components/Entity.js
+++ components/Entity.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
+import nlp from "compromise/two";
 
 import ViewSplitter from "../components/ViewSplitter";
 import { transcription } from "../data/transcription";
+let doc = nlp(transcription.transcript);
 
 import styles from "./Sentiments.module.css";
 
 const Entity = () => {
+  console.log(doc.tag("MaleName").out("tags"));
   return (
     <ViewSplitter>
       <div>
