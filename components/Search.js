6b13a3465ff53cb2f9ca0ac7993b325f432c34da
madzadev <madars.biss@gmail.com>
2022-03-31 22:44:37 +0200

    add input bar

--- components/Search.js
+++ components/Search.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 import ViewSplitter from "./ViewSplitter";
 import styles from "./Search.module.css";
 
 const Search = () => {
+  const [input, setInput] = useState("");
   return (
     <ViewSplitter>
       <div>
         <h1>Search</h1>
+        <input
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
       </div>
       <div>
-        <h1>Type at least 3 chars to get cues</h1>
+        {input.length > 2 ? (
+          <div>
+            <h1>Query: {input}</h1>
+          </div>
+        ) : (
+          <h1>Type at least 3 chars to get cues</h1>
+        )}
       </div>
     </ViewSplitter>
   );
