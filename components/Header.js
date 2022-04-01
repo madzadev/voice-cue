fac41bbc682a837e9fd0c2110c627a075984bd8f
madzadev <madars.biss@gmail.com>
2022-04-01 09:53:10 +0200

    add header home

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,21 @@
-import styles from "./Header.module.css";
 import Link from "next/link";
+import Router from "next/router";
+
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.wrapper}>
       <Link href="/">
-        <h2 className={styles.logo}>HotCues</h2>
+        <h2
+          className={styles.logo}
+          onClick={() => {
+            window.location.pathname === "/" &&
+              Router.reload(window.location.pathname);
+          }}
+        >
+          HotCues
+        </h2>
       </Link>
       <Link href="/features">
         <p className={styles.link}>Features</p>
