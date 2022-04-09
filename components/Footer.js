1719170dcfc4d68f53066240a324d456030601af
madzadev <madars.biss@gmail.com>
2022-04-09 23:19:34 +0200

    edit footer links

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,11 @@ const Footer = () => {
       <p className={styles.author}>
         Made with ❤️ by{" "}
         <span className={styles.link}>
-          <a href="https://github.com/madzadev" target="_blank">
+          <a
+            href="https://github.com/madzadev"
+            target="_blank"
+            rel="noreferrer"
+          >
             Madza
           </a>
         </span>
@@ -14,19 +18,19 @@ const Footer = () => {
       <p>
         Powered by{" "}
         <span className={styles.link}>
-          <a href="https://deepgram.com" target="_blank">
+          <a href="https://deepgram.com" target="_blank" rel="noreferrer">
             Deepgram
           </a>
         </span>
         ,{" "}
         <span className={styles.link}>
-          <a href="https://nextjs.org" target="_blank">
+          <a href="https://nextjs.org" target="_blank" rel="noreferrer">
             NextJS
           </a>
         </span>{" "}
         and{" "}
         <span className={styles.link}>
-          <a href="https://vercel.com" target="_blank">
+          <a href="https://vercel.com" target="_blank" rel="noreferrer">
             Vercel
           </a>
         </span>
